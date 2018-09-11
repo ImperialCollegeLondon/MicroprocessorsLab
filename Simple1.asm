0df4033c7a2b5b0e263a21a177c1fdd80ac0450f
Mark Neil <mark.neil@imperial.ac.uk>
2018-09-11 19:14:29 +0100

    Creating SImple_Interrupt branch

--- Simple1.asm
+++ Simple1.asm
@@ -1,21 +1,22 @@
 	#include p18f87k22.inc
-	
-	code
-	org 0x0
+
+rst	code	0x0000	; reset vector
 	goto	start
 	
-	org 0x100		    ; Main code starts here at address 0x100
+int_hi	code	0x0008	; high vector, no low vector
+	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
+	retfie	FAST		; if not then return
+	incf	LATD		; increment PORTD
+	bcf	INTCON,TMR0IF	; clear interrupt flag
+	retfie	FAST		; fast return from interrupt
 
-start
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-	
+main	code
+start	clrf	TRISD		; Set PORTD as all outputs
+	clrf	LATD		; Clear PORTD outputs
+	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
+	movwf	T0CON		; = 62.5KHz clock rate, approx 1sec rollover
+	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
+	bsf	INTCON,GIE	; Enable all interrupts
+	goto	$		; Sit in infinite loop
+
 	end
