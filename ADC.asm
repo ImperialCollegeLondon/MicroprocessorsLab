990a3117181e039bc6fecfc5a69730315782169f
Mark Neil <mark.neil@imperial.ac.uk>
2018-09-19 13:56:11 +0100

    Created ADC_test_measurement branch

---
+++ ADC.asm
@@ -0,0 +1,25 @@
+#include p18f87k22.inc
+
+    global  ADC_Setup, ADC_Read
+    
+ADC    code
+    
+ADC_Setup
+    bsf	    TRISA,RA0	    ; use pin A0(==AN0) for input
+    bsf	    ANCON0,ANSEL0   ; set A0 to analog
+    movlw   0x01	    ; select AN0 for measurement
+    movwf   ADCON0	    ; and turn ADC on
+    movlw   0x30	    ; Select 4.096V positive reference
+    movwf   ADCON1	    ; 0V for -ve reference and -ve input
+    movlw   0xF6	    ; Right justified output
+    movwf   ADCON2	    ; Fosc/64 clock and acquisition times
+    return
+
+ADC_Read
+    bsf	    ADCON0,GO	    ; Start conversion
+adc_loop
+    btfsc   ADCON0,GO	    ; check to see if finished
+    bra	    adc_loop
+    return
+
+    end
