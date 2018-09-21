5a91f4dd38d88e2f26f40e628364d05c249c9281
Mark Neil <mark.neil@imperial.ac.uk>
2018-09-21 15:52:22 +0100

    updated main and LCD files

--- LCD.asm
+++ LCD.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global  LCD_Setup, LCD_Write_Message
+    global  LCD_Setup, LCD_Write_Message, LCD_Write_Hex
 
 acs0    udata_acs   ; named variables in access ram
 LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
@@ -8,6 +8,9 @@ LCD_cnt_h   res 1   ; reserve 1 byte for variable LCD_cnt_h
 LCD_cnt_ms  res 1   ; reserve 1 byte for ms counter
 LCD_tmp	    res 1   ; reserve 1 byte for temporary use
 LCD_counter res 1   ; reserve 1 byte for counting through nessage
+
+acs_ovr	access_ovr
+LCD_hex_tmp res 1   ; reserve 1 byte for variable LCD_hex_tmp	
 
 	constant    LCD_E=5	; LCD enable bit
     	constant    LCD_RS=4	; LCD register select bit
@@ -46,6 +49,22 @@ LCD_Setup
 	call	LCD_delay_x4us
 	return
 
+LCD_Write_Hex	    ; Writes byte stored in W as hex
+	movwf	LCD_hex_tmp
+	swapf	LCD_hex_tmp,W	; high nibble first
+	call	LCD_Hex_Nib
+	movf	LCD_hex_tmp,W	; then low nibble
+LCD_Hex_Nib	    ; writes low nibble as hex character
+	andlw	0x0F
+	movwf	LCD_tmp
+	movlw	0x0A
+	cpfslt	LCD_tmp
+	addlw	0x07	; number is greater than 9 
+	addlw	0x26
+	addwf	LCD_tmp,W
+	call	LCD_Send_Byte_D ; write out ascii
+	return
+	
 LCD_Write_Message	    ; Message stored at FSR2, length stored in W
 	movwf   LCD_counter
 LCD_Loop_message
--- Simple1.asm
+++ Simple1.asm
@@ -1,7 +1,9 @@
 	#include p18f87k22.inc
 
-	extern	UART_Setup, UART_Transmit_Message  ; external UART subroutines
+	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
 	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
+	extern	LCD_Write_Hex			    ; external LCD subroutines
+	extern  ADC_Setup, ADC_Read		    ; external ADC routines
 	
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
@@ -24,6 +26,7 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
+	call	ADC_Setup	; setup ADC
 	goto	start
 	
 	; ******* Main programme ****************************************
@@ -48,8 +51,14 @@ loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movlw	myTable_l	; output message to UART
 	lfsr	FSR2, myArray
 	call	UART_Transmit_Message
-
-	goto	$		; goto current line in code
+	
+measure_loop
+	call	ADC_Read
+	movf	ADRESH,W
+	call	LCD_Write_Hex
+	movf	ADRESL,W
+	call	LCD_Write_Hex
+	goto	measure_loop		; goto current line in code
 
 	; a delay subroutine if you need one, times around loop in delay_count
 delay	decfsz	delay_count	; decrement until zero
