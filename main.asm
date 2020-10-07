b75d7ceedf13ad0687dac0b65a3af60757bcfb8c
Mark Neil <mark.neil@imperial.ac.uk>
2020-10-07 14:42:57 +0100

    some layout changes

--- main.asm
+++ main.asm
@@ -1,21 +1,23 @@
 	#include <pic18_chip_select.inc>
-	
+
 PSECT code
 	org 0x0
 	goto	start
-	
+
 	org 0x100		    ; Main code starts here at address 0x100
 
 start:
 	movlw 	0x0
 	movwf	TRISB, A	    ; Port C all outputs
 	bra 	test
-loop:	movff 	0x06, PORTB
+loop:
+	movff 	0x06, PORTB
 	incf 	0x06, W, A
-test:	movwf	0x06, A	    ; Test for end of loop condition
+test:
+	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0x63
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
-	
+
 	end
