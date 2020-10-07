ea629e262fe53fe2d2981d5b6e289d2b2ecb9820
Mark Neil <mark.neil@imperial.ac.uk>
2020-10-07 14:11:03 +0100

    Changes to project so that it compiles with mplabx5.4 and xc8 2.3 assembler

--- Simple1.asm
+++ Simple1.asm
@@ -1,20 +1,20 @@
-	#include p18f87k22.inc
+	#include <pic18_chip_select.inc>
 	
-	code
+PSECT code
 	org 0x0
 	goto	start
-	
-	org 0x100		    ; Main code starts here at address 0x100
+;	
+;	org 0x100		    ; Main code starts here at address 0x100
 
-start
+start:
 	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movwf	TRISB, A	    ; Port C all outputs
 	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
+loop:	movff 	0x06, PORTB
+	incf 	0x06, W, A
+test:	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
+	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 	
