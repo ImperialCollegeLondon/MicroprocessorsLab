cedb2ee6182dfc7e182cdf9de5d2305aac270809
Mark Neil <mark.neil@imperial.ac.uk>
2020-10-20 10:28:28 +0100

    Compiles for V5.4 - memory not correct yet

---
+++ UART.s
@@ -0,0 +1,37 @@
+#include <pic18_chip_select.inc>
+    
+global  UART_Setup, UART_Transmit_Message
+
+psect	udata_acs   ; reserve data space in access ram
+UART_counter: ds    1	    ; reserve 1 byte for variable UART_counter
+
+psect	code
+UART_Setup:
+    bsf	    SPEN	; enable
+    bcf	    SYNC	; synchronous
+    bcf	    BRGH	; slow speed
+    bsf	    TXEN	; enable transmit
+    bcf	    BRG16	; 8-bit generator only
+    movlw   103		; gives 9600 Baud rate (actually 9615)
+    movwf   SPBRG1, A	; set baud rate
+    bsf	    TRISC6	; TX1 pin is output on RC6 pin so must set TRISC6 to 1
+    return
+
+UART_Transmit_Message:	    ; Message stored at FSR2, length stored in W
+    movwf   UART_counter, A
+UART_Loop_message:
+    movf    POSTINC2, W, A
+    call    UART_Transmit_Byte
+    decfsz  UART_counter, A
+    bra	    UART_Loop_message
+    return
+
+UART_Transmit_Byte:	    ; Transmits byte stored in W
+    btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
+    bra	    UART_Transmit_Byte
+    movwf   TXREG1, A
+    return
+
+    end	    
+
+
---
+++ main.s
@@ -0,0 +1,55 @@
+#include <pic18_chip_select.inc>
+#include <xc.inc>
+
+extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
+	
+psect	udata_acs   ; reserve data space in access ram
+counter:    ds 1    ; reserve one byte for a counter variable
+delay_count:ds 1    ; reserve one byte for counter in the delay routine
+    
+psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
+myArray:    ds 0x80 ; reserve 128 bytes for message data
+
+psect	code, abs
+main: 	org 0x0
+ 	goto	setup
+	
+	; ******* myTable, data in programme memory, and its length *****
+myTable:
+	db	'H','e','l','l','o',' ','W','o','r','l','d','!','n'
+					; message, plus carriage return
+	myTable_l   EQU	13	; length of data
+	align	2
+	; ******* Programme FLASH read Setup Code ***********************
+setup:	bcf	CFGS	; point to Flash program memory  
+	bsf	EEPGD 	; access Flash program memory
+	call	UART_Setup	; setup UART
+	goto	start
+	
+	; ******* Main programme ****************************************
+start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
+	movlw	low highword(myTable)	; address of data in PM
+	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
+	movlw	high(myTable)	; address of data in PM
+	movwf	TBLPTRH, A		; load high byte to TBLPTRH
+	movlw	low(myTable)	; address of data in PM
+	movwf	TBLPTRL, A		; load low byte to TBLPTRL
+	movlw	myTable_l	; bytes to read
+	movwf 	counter, A		; our counter register
+loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	decfsz	counter, A		; count down to zero
+	bra	loop		; keep going until finished
+		
+	movlw	myTable_l	; output message to UART
+	lfsr	2, myArray
+	call	UART_Transmit_Message
+
+	goto	$		; goto current line in code
+
+	; a delay subroutine if you need one, times around loop in delay_count
+delay:	decfsz	delay_count, A	; decrement until zero
+	bra	delay
+	return
+
+	end	main
