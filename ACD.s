0b1c3147604c0fbc2d04133a48d0e1c421465ef6
Mark Neil <mark.neil@imperial.ac.uk>
2021-06-22 15:09:26 +0100

    Added correct BSR for ANSEL in ACD.s

--- ACD.s
+++ ACD.s
@@ -6,7 +6,9 @@ psect	adc_code, class=CODE
     
 ADC_Setup:
 	bsf	TRISA, PORTA_RA0_POSN, A  ; pin RA0==AN0 input
+	movlb	0x0f
 	bsf	ANSEL0	    ; set AN0 to analog
+	movlb	0x00
 	movlw   0x01	    ; select AN0 for measurement
 	movwf   ADCON0, A   ; and turn ADC on
 	movlw   0x30	    ; Select 4.096V positive reference
