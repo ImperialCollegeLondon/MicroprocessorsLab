f4957efdf0b47eb4b864d7333e6584ae6fd36515
pc719 <pc719@skbl-422-013.ph.ic.ac.uk>
2021-11-09 09:24:24 +0000

    PORTD counter w/ delay

--- main.s
+++ main.s
@@ -11,7 +11,7 @@ start:
 	movlw 	0x0		    ;sets W directory to 00 value
 	movwf	TRISC, A	    ; Port C all outputs
 	movlw	0xff		    ; sets W to all 1s
-	movwf	TRISD, A	    ; sets TRISD to akk 1s from W directory
+	movwf	TRISD, A	    ; sets TRISD to all 1s from W directory
 	bra 	test		    ; branches to test
 loop:
 	movff 	0x06, PORTC
@@ -24,7 +24,7 @@ test:
 	movwf	0x06, A	    ; Test for end of loop condition
 	movf	PORTD, W, A ;modifies program so that it changes no times loop is run depending on which switch button is pushed
 	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
+	bra 	loop		    ; Not yet finished go to start of loop again
 	goto 	0x0		    ; Re-run program from start
 
 delay:	DECFSZ 0x20, F, A	    
