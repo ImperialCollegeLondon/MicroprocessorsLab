172ff4ae4c4d2e05cd8b567386b686f243527eb8
Eichmeyer <lme19@ic.ac.uk>
2023-11-17 11:25:53 +0000

    keypad STuff

--- keypad.s
+++ keypad.s
@@ -1,38 +1,55 @@
 #include <xc.inc>
-    
-global  keypad_Setup, keypad_Transmit_Message
+  
+global  keypad_Setup, keypad_Read
 
 psect	udata_acs   ; reserve data space in access ram
 keypad_counter: ds    1	    ; reserve 1 byte for variable UART_counter
 
+psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
+keyval:    ds 0x80 ; reserve 128 bytes for message data
+        
 psect	keypad_code,class=CODE
+    
 keypad_Setup:
-    bsf	    SPEN	; enable
-    bcf	    SYNC	; synchronous
-    bcf	    BRGH	; slow speed
-    bsf	    TXEN	; enable transmit
-    bcf	    BRG16	; 8-bit generator only
-    movlw   103		; gives 9600 Baud rate (actually 9615)
-    movwf   SPBRG1, A	; set baud rate
-    bsf	    TRISC, PORTC_TX1_POSN, A	; TX1 pin is output on RC6 pin
-					; must set TRISC6 to 1
-					
+
+    movlb   15	; pad configure 1 is in 15?
+    bsf	    REPU    ; set pull-ups to on for PORTE
+    movlb   0
+    movlw   0x0F
+    clrf    LATE    ; write 0s to the lat e register
+
     return
 
-keypad_Transmit_Message:	    ; Message stored at FSR2, length stored in W
-    movwf   keypad_counter, A
-keypad_Loop_message:
-    movf    POSTINC2, W, A
-    call    keypad_Transmit_Byte
-    decfsz  keypad_counter, A
-    bra	    keypad_Loop_message
-    return
+    
+keypad_Read:
+   
+    call    bigdelay   ; can add a delay here to prevent need for status checks
+    movff   PORTE, 0xD ; num is low bits
+    movlw   0xF0    
+    movwf   TRISE   ; set TRISE to 0x0F
+    movf    PORTE, W ;  read PORTE to determine the logic levels on PORTE 0-3      
+    iorwf   0xE0, 0, 1   ; num is low bits
+    movwf   keyval
+    call    bigdelay 
+    goto    keypad_Read
 
-keypad_Transmit_Byte:	    ; Transmits byte stored in W
-    btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
-    bra	    keypad_Transmit_Byte
-    movwf   TXREG1, A
-    return
+
+delay:	decfsz	0xFF, A	; decrement until zero
+	bra	delay
+	return
+
+bigdelay:
+	call delay
+	call delay
+	call delay
+	call delay 
+	call delay
+	call delay
+	call delay
+	call delay
+	call delay 
+	call delay
+	return
 
 
 
