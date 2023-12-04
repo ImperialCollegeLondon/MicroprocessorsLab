2197c1abfa55dcd9971411f38f54831db1c630c4
Alex Coleman <111077126+alexncoleman@users.noreply.github.com>
2023-12-04 16:48:20 +0000

    Update RRInterval.s
    Created new file for working out period

--- RRInterval.s
+++ RRInterval.s
@@ -1,3 +1,14 @@
+#include <xc.inc>
 
 
-
+psect	External_timer, class = CODE
+ 
+ 
+ 
+RR_Setup:
+	bsf	CCP1CON, 0
+	bsf	CCP1CON, 2 ; sets up ECCP1 as capture on every rising edge
+	movlw	0x00
+	movwf	CCPTMRS0    ;sets ECCP1 to use timer1 for capture
+	movlw	10110011    ;enables Timer1 to synchronise with external clock at RA5 and perform read/write in 1 16bit operation
+	movwf	T1CON
