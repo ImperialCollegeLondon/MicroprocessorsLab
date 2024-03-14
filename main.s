d66fd1c06ef9961b143b17ce97f65a4993b97bb8
amg121 <amg121@skbl-418-005.ph.ic.ac.uk>
2024-03-14 11:43:47 +0000

    Added input code

--- main.s
+++ main.s
@@ -1,19 +1,22 @@
 #include <xc.inc>
 
-global	inputangle    
+global	inputangle, delay_ms, input_address_1, input_address_2  
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_Send_Byte_D, LCD_Clear
-extrn	Keypad_Setup, Keypad_Read, delay_ms
+extrn	LCD_Setup, LCD_Write_Message, LCD_Clear, Second_Line
+extrn	Keypad_Setup, Keypad_Read
 extrn	Input_Angle
+extrn	User_Input_Setup
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:	ds 1    ; reserve one byte for a counter variable
-delay_count:	ds 1    ; reserve one byte for counter in the delay routine
-input:		ds 1
-input_address	EQU 0xB0
-
-    inputangle	EQU 0xA0
+cnt_ms:	ds 1   ; reserve 1 byte for ms counter
+cnt_l:	ds 1   ; reserve 1 byte for variable cnt_l
+cnt_h:	ds 1   ; reserve 1 byte for variable cnt_h
+    
+    input_address_1	EQU 0xB0
+    input_address_2	EQU 0xC0
+    inputangle		EQU 0xA0
 	
 psect	udata_bank4 ; reserve data anywhere in RAM
 myArray:    ds 0x80
@@ -35,33 +38,43 @@ setup:	bcf	CFGS	; point to Flash program memory
 	
 	; ******* Main programme ****************************************
 start: 	
-	call	LCD_Clear
 	movlw	inputangle
 	movwf	FSR2
-	movlw	11
+	movlw	12
 	call	LCD_Write_Message
-	movlw	0xFF
 	call	delay_ms
-	goto	User_Input
-
-User_Input:
-	movlw	input_address
-	movwf	FSR0
-    
-	call	Keypad_Read ; finds button pressed and stores in WREG
-	movwf	input
-	movwf	PORTD
-	
-	movlw	0xFF
 	call	delay_ms
-
-	movlw	0x00
-	cpfslt	input
-	bra	User_Input
+	call	delay_ms
 	
-	movlw	input
-	call	LCD_Send_Byte_D ; writes what is stored in W to LCD
+	call	User_Input_Setup
 	goto	$
 
-	end	rst
 	
+;Delay Routines
+delay_ms:		    ; delay given in ms in W
+	movwf	cnt_ms, A
+lp2:	movlw	0xFF 
+	call	delay_x4us	
+	decfsz	cnt_ms, A
+	bra	lp2
+	return
+	
+delay_x4us:		    ; delay given in chunks of 4 microsecond in W
+	movwf	cnt_l, A	; now need to multiply by 16
+	swapf   cnt_l, F, A	; swap nibbles
+	movlw	0x0f	    
+	andwf	cnt_l, W, A ; move low nibble to W
+	movwf	cnt_h, A	; then to cnt_h
+	movlw	0xf0	    
+	andwf	cnt_l, F, A ; keep high nibble in cnt_l
+	call	delay
+	return
+
+delay:			; delay routine	4 instruction loop == 250ns	    
+	movlw 	0x00		; W=0
+lp1:	decf 	cnt_l, F, A	; no carry when 0x00 -> 0xff
+	subwfb 	cnt_h, F, A	; no carry when 0x00 -> 0xff
+	bc 	lp1		; carry, then loop again
+	return			; carry reset so return
+	
+	end	rst
