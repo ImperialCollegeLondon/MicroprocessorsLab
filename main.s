ebdc13a94842f00f813170c17ddb3c7b0dae1363
amg121 <amg121@skbl-418-005.ph.ic.ac.uk>
2024-03-14 11:58:02 +0000

    added comments

--- main.s
+++ main.s
@@ -8,17 +8,17 @@ extrn	Keypad_Setup, Keypad_Read
 extrn	Input_Angle
 extrn	User_Input_Setup
 	
-psect	udata_acs   ; reserve data space in access ram
-counter:	ds 1    ; reserve one byte for a counter variable
-cnt_ms:	ds 1   ; reserve 1 byte for ms counter
-cnt_l:	ds 1   ; reserve 1 byte for variable cnt_l
-cnt_h:	ds 1   ; reserve 1 byte for variable cnt_h
+psect	udata_acs			   ; reserve data space in access ram
+counter:	ds 1			    
+cnt_ms:	ds 1				   ; reserve 1 byte for ms counter
+cnt_l:	ds 1				   ; reserve 1 byte for variable cnt_l
+cnt_h:	ds 1				   ; reserve 1 byte for variable cnt_h
     
     input_address_1	EQU 0xB0
     input_address_2	EQU 0xC0
     inputangle		EQU 0xA0
 	
-psect	udata_bank4 ; reserve data anywhere in RAM
+psect	udata_bank4			   ; reserve data anywhere in RAM
 myArray:    ds 0x80
 
 psect	code, abs	
@@ -26,19 +26,19 @@ rst: 	org 0x0
  	goto	setup
 
 	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup LCD
+setup:	bcf	CFGS			   ; point to Flash program memory  
+	bsf	EEPGD			   ; access Flash program memory
+	call	UART_Setup		   ; setup UART
+	call	LCD_Setup		   ; setup LCD
 	call	Keypad_Setup
 	
-	call	Input_Angle  ; load message
+	call	Input_Angle		   ; load message
 	
 	goto	start
 	
 	; ******* Main programme ****************************************
 start: 	
-	movlw	inputangle  ; Writes 'input angle' messge 
+	movlw	inputangle		   ; Writes 'input angle' messge 
 	movwf	FSR2
 	movlw	12
 	call	LCD_Write_Message  
@@ -46,12 +46,13 @@ start:
 	call	delay_ms
 	call	delay_ms
 	
-	call	User_Input_Setup    ; Waits for user input (8-bit/2-digits)
+	call	User_Input_Setup	  ; Waits for user input 
+					  ; (8-bit/2-digits)
 	goto	$
 
 	
 ;Delay Routines
-delay_ms:		    ; delay given in ms in W
+delay_ms:				  ; delay given in ms in W
 	movwf	cnt_ms, A
 lp2:	movlw	0xFF 
 	call	delay_x4us	
@@ -59,22 +60,23 @@ lp2:	movlw	0xFF
 	bra	lp2
 	return
 	
-delay_x4us:		    ; delay given in chunks of 4 microsecond in W
-	movwf	cnt_l, A	; now need to multiply by 16
-	swapf   cnt_l, F, A	; swap nibbles
+delay_x4us:				; delay given in chunks of 
+					; 4 microsecond in W
+	movwf	cnt_l, A		; now need to multiply by 16
+	swapf   cnt_l, F, A		; swap nibbles
 	movlw	0x0f	    
-	andwf	cnt_l, W, A ; move low nibble to W
-	movwf	cnt_h, A	; then to cnt_h
+	andwf	cnt_l, W, A		; move low nibble to W
+	movwf	cnt_h, A		; then to cnt_h
 	movlw	0xf0	    
-	andwf	cnt_l, F, A ; keep high nibble in cnt_l
+	andwf	cnt_l, F, A		; keep high nibble in cnt_l
 	call	delay
 	return
 
-delay:			; delay routine	4 instruction loop == 250ns	    
-	movlw 	0x00		; W=0
-lp1:	decf 	cnt_l, F, A	; no carry when 0x00 -> 0xff
-	subwfb 	cnt_h, F, A	; no carry when 0x00 -> 0xff
-	bc 	lp1		; carry, then loop again
-	return			; carry reset so return
+delay:					; delay routine	4 instruction loop    
+	movlw 	0x00			; W=0
+lp1:	decf 	cnt_l, F, A		; no carry when 0x00 -> 0xff
+	subwfb 	cnt_h, F, A		; no carry when 0x00 -> 0xff
+	bc 	lp1			; carry, then loop again
+	return				; carry reset so return
 	
 	end	rst
