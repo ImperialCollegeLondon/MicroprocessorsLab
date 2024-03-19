4ee944a0df5f80ef689aa97928ffcc4378097184
amg121 <amg121@skbl-418-005.ph.ic.ac.uk>
2024-03-19 11:08:17 +0000

    Added sine and cosine messages

--- Messages.s
+++ Messages.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
     
-global	Input_Angle
-extrn	inputangle
+global	Input_Angle, Sine_Msg, Cosine_Msg
+extrn	inputangle, sine, cosine
     
 psect Messages, class = CODE
  
@@ -57,4 +57,62 @@ Input_Angle:
 	movwf	INDF0
 	
 	return 
+	
+Cosine_Msg:
+    movlw   cosine
+    movwf   FSR0
+    
+    movlw   'C'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'o'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   's'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'i'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'n'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'e'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   ':'
+    movwf   INDF0
+    
+    return 
+    
+Sine_Msg:
+    movlw   sine
+    movwf   FSR0
+    
+    movlw   'S'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'i'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'n'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   'e'
+    movwf   INDF0
+    incf    FSR0, F
+    
+    movlw   ':'
+    movwf   INDF0
+    
+    return 
 
