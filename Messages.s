c20b4322256efd13588159a03353d875b5c9c437
amg121 <amg121@skbl-418-005.ph.ic.ac.uk>
2024-03-19 11:11:26 +0000

    Reformatted

--- Messages.s
+++ Messages.s
@@ -6,57 +6,57 @@ extrn	inputangle, sine, cosine
 psect Messages, class = CODE
  
 Input_Angle: 
-	movlw	inputangle
-	movwf	FSR0	; points to start of message
-	
-	movlw	'I'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'n'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'p'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'u'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	't'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	' '
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'A'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'n'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'g'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'l'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	'e'
-	movwf	INDF0
-	incf	FSR0, F
-	
-	movlw	':'
-	movwf	INDF0
-	
-	return 
+    movlw   inputangle
+    movwf   FSR0		; points to start of message
+
+    movlw   'I'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'n'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'p'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'u'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   't'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   ' '
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'A'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'n'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'g'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'l'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   'e'
+    movwf   INDF0
+    incf    FSR0, F
+
+    movlw   ':'
+    movwf   INDF0
+
+    return 
 	
 Cosine_Msg:
     movlw   cosine
