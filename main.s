904f88a3a1c65fcf00cbda202b05f2932f97f926
yyc22 <yyc22@skbl-418-007.ph.ic.ac.uk>
2024-10-31 11:50:39 +0000

    Changed movlw 0x63 to movf PORTD,W,A

--- main.s
+++ main.s
@@ -16,7 +16,7 @@ loop:
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movf    PORTD, W, A
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
