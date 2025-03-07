105117a03dc8fb785863da918b55c66f243c11b5
Hollister <lh21@ic.ac.uk>
2025-03-07 10:11:12 +0000

    working printing

--- main.s
+++ main.s
@@ -1,4 +1,4 @@
-CONFIG  XINST = OFF           ; Extended Instruction Set (Disabled)
+
 #include <xc.inc>
 
 extrn LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_I, LCD_delay_ms, LCD_Send_Byte_D
@@ -6,7 +6,6 @@ extrn LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_I, LCD_delay_ms
 psect	udata_acs   ; reserve data space in access ram
 counter_pt:	ds 1    ; counter for printing the initial data
 counter_ec:	ds 1	; encoding counter
-next_address:    ds 1	; store the nex adrdress to write to
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 PlaintextArray:    ds 0x80 ; reserve 128 bytes for message data
@@ -19,11 +18,6 @@ PlaintextTable:
 	db	'P','l','a','i','n','t','e','x','t'
 					
 	TableLength   EQU	9	
-	align	2
-
-
-CiphertextTable:
-	db	'a','a','a', 'a','a','a','a','a','a'
 	align	2
 	
 psect	code, abs
@@ -41,86 +35,99 @@ setup:	bcf	CFGS	; point to Flash program memory
 	goto	start
 
 start:
-	call	setup_plaintext		; our counter register
-	call	print_message
 	
-	movlw	0x4F
+	call	print_plaintext		; print the plaintext
+	movlw   0xC0        ; Move the cursor to the second line (or wherever needed)
+	call    LCD_Send_Byte_I
+
+	movlw	0xFF	    ; allow time for cursor to move
 	call	LCD_delay_ms
 	
-	call    modify_table     
-	call	setup_ciphertext
-    	call	print_message
+	call modify_table        ; Modify the ciphertext array
+	call print_ciphertext    ; Print the modified data to the LCD
 	
-	movlw	0x4F
+	movlw	0xFF
+	call	LCD_delay_ms
+	movlw	0xFF
+	call	LCD_delay_ms
+	movlw	0xFF
+	call	LCD_delay_ms
+	movlw	0xFF
 	call	LCD_delay_ms
 	
 	goto	ending
-	
-setup_plaintext:
-	lfsr	0, PlaintextArray	; Load FSR0 with address in RAM	
-	movlw	low highword(PlaintextTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(PlaintextTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(PlaintextTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	TableLength	; bytes to read
-	movwf 	counter_pt, A
-	return
-	
-setup_ciphertext:
-	lfsr	0, CiphertextArray	; Load FSR0 with address in RAM	
-	movlw	low highword(CiphertextTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(CiphertextTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(CiphertextTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	TableLength	; bytes to read
-	movwf 	counter_pt, A
-	return
-		
-print_message: 	
-    	movlw	0x01
-	movwf	PORTH, A
-	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	movf	TABLAT, W, A
 
-	call	LCD_Send_Byte_D
-	movlw	0x00
-	movwf	PORTH, A
-	decfsz	counter_pt, A		; count down to zero
-	bra	print_message	; keep going until finished
-	
-	movlw	0xC0
-	call LCD_Send_Byte_I
-	return
-	
 modify_table:
-	movf CiphertextArray, W, A
-	movwf next_address, A
-	call setup_plaintext
-	goto modify_loop
-   
+    movlw   LOW(PlaintextArray)  ; Load low byte of PlaintextArray address
+    movwf   FSR1L, A
+    movlw   HIGH(PlaintextArray) ; Load high byte of PlaintextArray address
+    movwf   FSR1H, A
+
+    movlw   LOW(CiphertextArray) ; Load low byte of CiphertextArray address
+    movwf   FSR0L, A
+    movlw   HIGH(CiphertextArray); Load high byte of CiphertextArray address
+    movwf   FSR0H, A
+
+    movlw   TableLength          ; Load the number of characters to process
+    movwf   counter_ec, A        ; Store in counter_ec
+
+    goto    modify_loop          ; Start modification
+
 modify_loop:
-		
-	tblrd*+			    ; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0    ; move data from TABLAT to (FSR0), inc FSR0	
-	movf	TABLAT, W, A
-	movwf	next_address, A
-	incf	next_address, A
-	decfsz	counter_pt, A
-	bra modify_loop	
-	return
-	
+    movf    counter_ec, W, A     ; Check if counter is zero
+    bz      modify_done          ; If zero, we are done
 
+    movf    INDF1, W, A          ; Read character from PlaintextArray
+    movwf   INDF0, A             ; Write character to CiphertextArray
+
+    incf    FSR1L, A             ; Increment FSR1 (next character in PlaintextArray)
+    incf    FSR0L, A             ; Increment FSR0 (next character in CiphertextArray)
+
+    decfsz  counter_ec, A        ; Decrement counter and check if done
+    bra     modify_loop          ; Loop again if not finished
+
+modify_done:
     return
+    
+print_ciphertext:
+    ; Load the start address of CiphertextArray into FSR0
+    movlw   LOW(CiphertextArray)  
+    movwf   FSR0L, A
+    movlw   HIGH(CiphertextArray) 
+    movwf   FSR0H, A
 
+    movlw   TableLength    ; Load the number of characters
+    movwf   counter_pt, A  ; Store in counter
+    goto print_loop
+    
+print_plaintext:
+    ; Load the start address of PlaintextArray into FSR0
+    movlw   LOW(PlaintextArray)  
+    movwf   FSR0L, A
+    movlw   HIGH(PlaintextArray) 
+    movwf   FSR0H, A
+
+    movlw   TableLength    ; Load the number of characters to print
+    movwf   counter_pt, A  ; Store in counter
+    goto    print_loop 
+   
+print_loop:
+    movf    counter_pt, W, A  
+    bz      print_done      ; If counter is zero, we're done
+
+    movf    INDF0, W, A    ; Read a character from PlaintextArray
+    call    LCD_Send_Byte_D ; Send it to the LCD
+
+    incf    FSR0L, A       ; Move to the next character in PlaintextArray
+    decfsz  counter_pt, A  
+    bra     print_loop      ; Loop until all characters are printed
+    goto    print_done
+
+print_done:
+    return
 
 ending:
     nop
-    goto $
     
     end rst
     
