7bdb84234b60a684cd48f692f37b280c3b32a6bf
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-14 15:54:19 +0000

    Update main.s
    Changed file for one from lab today

--- main.s
+++ main.s
@@ -1,30 +1,16 @@
 #include <xc.inc>
 
-extrn	init_phase_accum, init_timer, update_phase, output_waveform, phase_jump
-	
-;===================================MAIN====================================
+extrn	Timer_Setup, Lookup_Setup, DDS_Int_Hi
+
 psect	code, abs
-org 0x0000
-    goto main      ; Ensure proper startup handling
+rst:	org	0x0000	; reset vector
+	goto	start
 
-org 0x0008
-    goto int_service   ; Define interrupt vector
+int_hi:	org	0x0008	; high vector, no low vector
+	goto	DDS_Int_Hi
+	
+start:	call	Timer_Setup
+	call	Lookup_Setup
+	goto	$	; Sit in infinite loop
 
-main:
-    call    init_phase_accum
-    call    init_timer
-    bsf     INTCON, 7,	A   ; Bit 7 in INTCON is GIE
-    goto    $
-;===================================ISR====================================
-int_service:
-    btfsc  PIR1, 0, A	    ; Bit 0 of PIR1 is TMR1IF
-    goto   handle_timer1
-    retfie		    ; Return if it wasn?t Timer1
-
-handle_timer1:
-    bcf    PIR1, 0, A	    ; Clear Timer1 interrupt flag
-    call   update_phase
-    call   output_waveform
-    retfie
-
-    end main
+	end	rst
