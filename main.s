24a66db0e45310c924e3aa15ca8711104869bf06
Rudy <125066362+Squidwater606@users.noreply.github.com>
2025-03-17 16:30:01 +0000

    Update main.s

--- main.s
+++ main.s
@@ -4,14 +4,14 @@ extrn	Phase_Setup, Timer_Setup, Lookup_Setup, DDS_Int_Hi
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
-	goto	start
+	goto	Start
 
-int_hi:	org	0x0008	; high vector, no low vector
+Int_Hi:	org	0x0008	; high vector, no low vector
 	goto	DDS_Int_Hi
 	
-start:	call	Phase_Setup
+Start:	call	Phase_Setup
 	call	Timer_Setup
 	call	Lookup_Setup
 	goto	$	; Sit in infinite loop
 
-	end	rst
+    end	    rst
