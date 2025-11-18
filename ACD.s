a6001a68fbd623c8942b1e14544dba9a714d8f54
ah2723 <ah2723@skbl-418-006.ph.ic.ac.uk>
2025-11-18 11:59:31 +0000

    successful conversion from hex to decimal, no output

--- ACD.s
+++ ACD.s
@@ -1,12 +1,15 @@
 #include <xc.inc>
 
-global  ADC_Setup, ADC_Read   
+global ADC_Setup, ADC_Read, ADC_Convert
+global RES0, RES1, RES2, RES3
+global DEC0, DEC1, DEC2, DEC3
  
 psect	udata_acs   ; reserve data space in access ram
-D1:    ds 1    ; reserve one byte for digit 1 of result
-D2:    ds 1    ; reserve one byte for digit 2
-D3:    ds 1    ; reserve one byte for digit 3
-D4:    ds 1    ; reserve one byte for digit 4
+DEC0:    ds 1    ; reserve one byte for digit 1 of result (output goes as D0 D1 D2 D3)
+DEC1:    ds 1    ; reserve one byte for digit 2 
+DEC2:    ds 1    ; reserve one byte for digit 3
+DEC3:    ds 1    ; reserve one byte for digit 4
+
     
 RES0:	ds 4
 RES1:	ds 4
@@ -15,6 +18,7 @@ RES3:	ds 4
 
 ARG1L:	ds 1
 ARG1H:	ds 1
+ARG1M:	ds 1
 ARG2L:	ds 1
 ARG2H:	ds 1 
 
@@ -40,8 +44,145 @@ adc_loop:
 	bra	adc_loop
 	return
 
-ADC_Convert:
-	k	EQU   0x418A ; conversion factor k
+ADC_Convert:	; requires ADC_Read to be called first
+	; k is 0x418A - conversion factor k
+	movlw	0x41
+	movwf	ARG2H, A
+	movlw	0x8A
+	movwf	ARG2L, A
+	movff	ADRESH, ARG1H, A
+	movff	ADRESL, ARG1L, A
+	
+	; begin conversion via 16 x 16 unsigned multiply routine, see datasheet
+	movf	ARG1L, W
+	mulwf	ARG2L	
+	
+	movff	PRODH, RES1
+	movff	PRODL, RES0
+	
+	movf	ARG1H, W
+	mulwf	ARG2H
+	
+	movff	PRODH, RES3
+	movff	PRODL, RES2
+	
+	movf	ARG1L, W
+	mulwf	ARG2H
+	
+	movf	PRODL, W
+	addwf	RES1, F
+	movf	PRODH, W
+	addwfc	RES2, F
+	clrf	WREG
+	addwfc	RES3, F
+	
+	movf	ARG1H, W
+	mulwf	ARG2L
+	
+	movf	PRODL, W
+	addwf	RES1, F
+	movf	PRODH, W
+	addwfc	RES2, F
+	clrf	WREG
+	addwfc	RES3, F
+	
+	; setup for second multiplication
+	movff	RES3, DEC0, A ; save result of step 1 to first digit 
+	
+	movff	RES0, ARG1L, A ; save other results to be used in further multiplications
+	movff	RES1, ARG1M, A
+	movff	RES2, ARG1H, A
+	
+	movlw	0x0A	; mini_k constant for finding less significant bits
+	movwf	ARG2L, A
+	clrf	ARG2H
+	
+	; commence second multiplication
+	
+	movf	ARG1L, W, A
+	mulwf	ARG2L
+	
+	movff	PRODH, RES1
+	movff	PRODL, RES0
+	
+	movf	ARG1H, W, A
+	mulwf	ARG2L
+
+	movff	PRODH, RES3
+	movff	PRODL, RES2
+	
+	movf	ARG1M, W, A
+	mulwf	ARG2L
+	
+	movf	PRODL, W
+	addwf	RES1, F
+	movf	PRODH, W
+	addwfc	RES2, F
+	clrf	WREG
+	addwfc	RES3, F
+	
+	movff	RES3, DEC1, A ; save useful result
+	
+	; setup for third multiplication
+	movff	RES0, ARG1L, A ; save other results to be used in further multiplications
+	movff	RES1, ARG1M, A
+	movff	RES2, ARG1H, A
+	
+	; commence third multiplication
+	movf	ARG1L, W, A
+	mulwf	ARG2L
+	
+	movff	PRODH, RES1
+	movff	PRODL, RES0
+	
+	movf	ARG1H, W, A
+	mulwf	ARG2L
+
+	movff	PRODH, RES3
+	movff	PRODL, RES2
+	
+	movf	ARG1M, W, A
+	mulwf	ARG2L
+	
+	movf	PRODL, W
+	addwf	RES1, F
+	movf	PRODH, W
+	addwfc	RES2, F
+	clrf	WREG
+	addwfc	RES3, F
+	
+	movff	RES3, DEC2, A ; save useful result
+	
+	; setup for fourth multiplication
+	movff	RES0, ARG1L, A ; save other results to be used in further multiplications
+	movff	RES1, ARG1M, A
+	movff	RES2, ARG1H, A
+	
+	; commence fourth multiplication
+	movf	ARG1L, W, A
+	mulwf	ARG2L
+	
+	movff	PRODH, RES1
+	movff	PRODL, RES0
+	
+	movf	ARG1H, W, A
+	mulwf	ARG2L
+
+	movff	PRODH, RES3
+	movff	PRODL, RES2
+	
+	movf	ARG1M, W, A
+	mulwf	ARG2L
+	
+	movf	PRODL, W
+	addwf	RES1, F
+	movf	PRODH, W
+	addwfc	RES2, F
+	clrf	WREG
+	addwfc	RES3, F
+	
+	movff	RES3, DEC3, A ; save useful result
+	
 	
 	
 end
--- main.s
+++ main.s
@@ -2,7 +2,7 @@
 
 extrn	UART_Setup, UART_Transmit_Message  ; external uart subroutines
 extrn	LCD_Setup, LCD_Write_Message, LCD_Write_Hex ; external LCD subroutines
-extrn	ADC_Setup, ADC_Read		   ; external ADC subroutines
+extrn	ADC_Setup, ADC_Read, ADC_Convert		   ; external ADC subroutines
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
@@ -57,6 +57,7 @@ loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	
 measure_loop:
 	call	ADC_Read
+	call	ADC_Convert
 	movf	ADRESH, W, A
 	call	LCD_Write_Hex
 	movf	ADRESL, W, A
