d59a31527149e68d0315088d3110451f52903e9d
Mark Neil <mark.neil@imperial.ac.uk>
2026-05-13 16:56:42 +0100

    Updated to run on vscode

--- DAC_Interrupt.s
+++ DAC_Interrupt.s
@@ -6,18 +6,18 @@ psect	dac_code, class=CODE
 	
 DAC_Int_Hi:	
 	btfss	TMR0IF		; check that this is timer0 interrupt
-	retfie	f		; if not then return
+	retfie	f			; if not then return
 	incf	LATJ, F, A	; increment PORTD
-	bcf	TMR0IF		; clear interrupt flag
-	retfie	f		; fast return from interrupt
+	bcf		TMR0IF		; clear interrupt flag
+	retfie	f			; fast return from interrupt
 
 DAC_Setup:
 	clrf	TRISJ, A	; Set PORTD as all outputs
 	clrf	LATJ, A		; Clear PORTD outputs
 	movlw	10000111B	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON, A	; = 62.5KHz clock rate, approx 1sec rollover
-	bsf	TMR0IE		; Enable timer0 interrupt
-	bsf	GIE		; Enable all interrupts
+	bsf		TMR0IE		; Enable timer0 interrupt
+	bsf		GIE			; Enable all interrupts
 	return
 	
 	end
--- main.s
+++ main.s
@@ -9,7 +9,8 @@ rst:	org	0x0000	; reset vector
 int_hi:	org	0x0008	; high vector, no low vector
 	goto	DAC_Int_Hi
 	
-start:	call	DAC_Setup
+start:	
+	call	DAC_Setup
 	goto	$	; Sit in infinite loop
 
 	end	rst
